57f9e12e6a0accddb535667760ca1779cc01f0e9
David Sudia <dsudia@gmail.com>
2016-05-26 14:39:57 -0600

    algorithm runner draft complete

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,93 @@
-var express = require('express');
-var router = express.Router();
-var mongojs = require('mongojs');
+const express = require('express');
+const router = express.Router();
+const pmongo = require('promised-mongo');
+const matchAlgorithm = require('../algorithm/algorithm');
+const config = require('../config');
 
 // setup Mongo
-var defaultMongoPort = 27017;
-var host = process.env.MD_PORT_27017_TCP_ADDR;
+const defaultMongoPort = 27017;
+let host = process.env.MD_PORT_27017_TCP_ADDR;
 if (host === null) {
     host = 'localhost';
 }
-var port = process.env.MD_PORT_27017_TCP_PORT;
+let port = process.env.MD_PORT_27017_TCP_PORT;
 if (port === null) {
     port = defaultMongoPort;
 }
-var url = 'mongodb://' + host + ':' + port + '/potentialMatches';
-var db = mongojs(url, ['potentialMatches']);
+let url = 'mongodb://' + host + ':' + port + '/potentialMatches';
+const db = pmongo(url, ['potentialMatches']);
 
 
 // Run matches
-router.post('/notify', function(req, res, next) {
-    var userEmail = req.body.email;
-    var update = req.body.update;
-    // remove deactivated users from all match arrays if update is false
-    if (update === false) {
-        var allUsers = db.potentialMatches.find({
-          email: {$ne: userEmail}
-        });
-        db.potentialMatches.findOne({
-            email: userEmail
-        }, (err, usersProfile) => {
-            
+router.post('/notify', (req, res, next) => {
+    const userEmail = req.body.email;
+    const update = req.body.update;
+    let allUsers = [];
+    // run matching algorithm if update is true
+    if (update === true) {
+        let currentEmail = "";
+        let matchPercent = 0;
+        return db.potentialMatches.find({
+                $ne: userEmail
+            }).toArray()
+            .then(docs => {
+                allUsers = docs;
+                return db.potentialMatches.findOne({
+                        email: userEmail
+                    })
+                    .then(userProfile => {
+                        allUsers.forEach(currentProfile => {
+                            currentEmail = currentProfile.email;
+                            matchPercent = matchAlgorithm(userProfile, currentProfile);
+                            if (matchPercent >= config.cutoff) {
+                                return db.potentialMatches.findAndModify({
+                                        query: {
+                                            email: userEmail
+                                        },
+                                        update: {
+                                            $push: {
+                                                matchSuggestions: {
+                                                    email: currentEmail,
+                                                    perc: matchPercent
+                                                }
+                                            }
+                                        },
+                                        new: true
+                                    })
+                                    .then(updatedProf => {
+                                        return db.potentialMatches.findAndModify({
+                                            query: {
+                                                email: currentemail
+                                            },
+                                            update: {
+                                                $push: {
+                                                    matchSuggestions: {
+                                                        email: userEmail,
+                                                        perc: matchPercent
+                                                    }
+                                                }
+                                            }
+                                        })
+                                    })
+                            } else {
+                                return;
+                            }
+                        })
+                    })
+            })
+    } else {
+        // remove deactivated users from all match arrays if update is false
+        return db.potentialMatches.update({
+            $pull: {
+                matchSuggestions: {
+                    email: userEmail,
+                    perc: {
+                        $gt: 0
+                    }
+                }
+            }
         })
     }
-
-
-    // run matching algorithm if update is true
-
 });
 
 module.exports = router;
