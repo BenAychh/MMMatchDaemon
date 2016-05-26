7c4fce2f81870860516eb1fa90bd0a477a6aa753
David Sudia <dsudia@gmail.com>
2016-05-26 13:59:59 -0600

    removed mongoose config

--- config.js
+++ config.js
@@ -2,10 +2,4 @@ module.exports = {
 
     cutoff: 80,
 
-    mongoURI: {
-        test: 'mongodb://localhost/potentialMatches-testing',
-        development: 'mongodb://localhost/potentialMatches',
-        production: process.env.MONGODB_URI
-    }
-
 }
