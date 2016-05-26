f9387298068d774a15624ecd40d11ba2b56a0c9f
David Sudia <dsudia@gmail.com>
2016-05-26 13:32:46 -0600

    switched to mongojs for syntax

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
 
 // setup Mongo
-var MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
-
 var defaultMongoPort = 27017;
 var host = process.env.MD_PORT_27017_TCP_ADDR;
 if (host === null) {
@@ -15,14 +13,25 @@ if (port === null) {
     port = defaultMongoPort;
 }
 var url = 'mongodb://' + host + ':' + port + '/potentialMatches';
+var db = mongojs(url, ['potentialMatches']);
 
 
 // Run matches
 router.post('/notify', function(req, res, next) {
-    var user = req.body.email;
+    var userEmail = req.body.email;
     var update = req.body.update;
     // remove deactivated users from all match arrays if update is false
-    
+    if (update === false) {
+        var allUsers = db.potentialMatches.find({
+          email: {$ne: userEmail}
+        });
+        db.potentialMatches.findOne({
+            email: userEmail
+        }, (err, usersProfile) => {
+            
+        })
+    }
+
 
     // run matching algorithm if update is true
 
