d6552bf66739cc18930ff7b4a00da76cd849597a
David Sudia <dsudia@gmail.com>
2016-05-26 12:08:54 -0600

    added mongoURIs to config

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
 module.exports = {
 
-    cutoff: 80
+    cutoff: 80,
+
+    mongoURI: {
+        test: 'mongodb://localhost/potentialMatches-testing',
+        development: 'mongodb://localhost/potentialMatches',
+        production: process.env.MONGODB_URI
+    }
 
 }
