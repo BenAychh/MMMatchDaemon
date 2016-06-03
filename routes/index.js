3b24757704fd29b5cbb7ebda9df267f84a77d686
David Sudia <dsudia@gmail.com>
2016-06-03 14:52:06 -0600

    all tests passing

--- routes/index.js
+++ routes/index.js
@@ -30,49 +30,51 @@ router.post('/notify', (req, res, next) => {
                 return db.potentialMatches.find().toArray()
                     .then(docs => {
                         allUsers = docs;
-                        return allUsers.forEach((currentProfile, ind) => {
+                        return allUsers.forEach((currentProfile, ind, arr) => {
                             if (currentProfile.active === true && currentProfile.email !== userEmail) {
-                                currentEmail = currentProfile.email;
-                                matchPercent = Number(matchAlgorithm(userProfile, currentProfile));
-                                if (matchPercent >= config.cutoff) {
-                                    return db.potentialMatches.findAndModify({
-                                            query: {
-                                                email: userEmail
-                                            },
-                                            update: {
-                                                $push: {
-                                                    matchSuggestions: {
-                                                        email: currentEmail,
-                                                        perc: matchPercent
-                                                    }
-                                                }
-                                            },
-                                            new: true
-                                        })
-                                        .then(updatedProf => {
+                                if ((userProfile.isTeacher === true && currentProfile.isTeacher === false) || (userProfile.isTeacher === false && currentProfile.isTeacher === true)) {
+                                    try {
+                                        matchPercent = Number(matchAlgorithm(userProfile, currentProfile));
+                                    } catch (err) {
+                                        console.log(err);
+                                    }
+
+                                        if (matchPercent >= config.cutoff) {
                                             return db.potentialMatches.findAndModify({
                                                     query: {
-                                                        email: currentEmail
+                                                        email: userEmail
                                                     },
                                                     update: {
                                                         $push: {
                                                             matchSuggestions: {
-                                                                email: userEmail,
+                                                                email: currentProfile.email,
                                                                 perc: matchPercent
                                                             }
                                                         }
                                                     },
                                                     new: true
                                                 })
-                                                .then(updatedProfile => {
-                                                    return;
-                                                })
-                                        });
-                                } else {
-                                    return;
+                                                .then(updatedProf => {
+                                                    return db.potentialMatches.findAndModify({
+                                                            query: {
+                                                                email: currentProfile.email
+                                                            },
+                                                            update: {
+                                                                $push: {
+                                                                    matchSuggestions: {
+                                                                        email: userEmail,
+                                                                        perc: matchPercent
+                                                                    }
+                                                                }
+                                                            },
+                                                            new: true
+                                                        })
+                                                        .then(updatedProfile => {
+                                                            return;
+                                                        })
+                                                });
+                                        }
                                 }
-                            } else {
-                                return;
                             }
                         })
                     })
@@ -81,7 +83,7 @@ router.post('/notify', (req, res, next) => {
                             status: 200,
                             message: 'Match suggestions updated for ' + userEmail
                         })
-                    })
+                    });
             })
             .catch(err => {
                 res.status(400).json({
